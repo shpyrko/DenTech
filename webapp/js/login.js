3f55e015164b8ae33a4eb20b77aaa8b17d91d9f7
Alex Shpyrko <alex.shpyrko@gmail.com>
2021-03-04 15:53:23 -0500

    Began work on login page

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    var login = document.getElementById("login");
+    var username_input = document.getElementById("username_input");
+    var password_input = document.getElementById("password_input");
+    
+    $(login).click(function(){
+       $.post("/login", {
+           username: username_input,
+           password: password_input
+       },
+             function(){
+           // do something if request successful
+       }) 
+    });
 
 });
