71e4f8153d20e9c01f40c1070dda5dbd63e8868c
Alex Shpyrko <alex.shpyrko@gmail.com>
2021-03-06 17:12:36 -0500

    Added second page and basic logic for login

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
     var login = document.getElementById("login");
-    var username_input = document.getElementById("username_input");
-    var password_input = document.getElementById("password_input");
+    var username_input = document.getElementById("#username_input");
+    var password_input = document.getElementById("#password_input");
     
-    $(login).click(function(){
-       $.post("/login", {
+    $("#login").click(function(){
+       $.post("http://127.0.0.1:5000/webapp/loginAuth", {
            username: username_input,
            password: password_input
        },
-             function(){
-           // do something if request successful
-       }) 
+        function(data){
+           window.location.replace(data);
+       }),
+           "text"
     });
 
 });
